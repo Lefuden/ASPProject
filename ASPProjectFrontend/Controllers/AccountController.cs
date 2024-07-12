8d955837ffba264dfb442101a05b763c253e9b50
Lefuden <lefuden@gmail.com>
2024-07-12 17:50:20 +0200

    added logout button

--- ASPProjectFrontend/Controllers/AccountController.cs
+++ ASPProjectFrontend/Controllers/AccountController.cs
@@ -1,35 +1,44 @@
 ﻿using ASPProjectFrontend.Models;
 using ASPProjectFrontend.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASPProjectFrontend.Controllers;
 public class AccountController(ApiServices apiServices) : Controller
 {
-    private readonly ApiServices _api = apiServices;
+	private readonly ApiServices _api = apiServices;
 
-    [HttpGet]
-    public async Task<IActionResult> AddAddress()
-    {
-        return View(new Address());
-    }
+	[HttpGet]
+	public async Task<IActionResult> AddAddress()
+	{
+		return View(new Address());
+	}
 
-    [HttpPost]
-    public async Task<IActionResult> UpdateAddress(Address address)
-    {
-        var user = HttpContext.User;
-        var result = await _api.AddAddressToNewUser(address, user);
-        if (result)
-        {
-            return RedirectToAction("Index", "Home");
-        }
-        else
-        {
-            return RedirectToAction("AddAddress");
-        }
-    }
+	[HttpPost]
+	public async Task<IActionResult> UpdateAddress(Address address)
+	{
+		var user = HttpContext.User;
+		var result = await _api.AddAddressToNewUser(address, user);
+		if (result)
+		{
+			return RedirectToAction("Index", "Home");
+		}
+		else
+		{
+			return RedirectToAction("AddAddress");
+		}
+	}
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+	[HttpGet]
+	public async Task<IActionResult> Logout()
+	{
+		await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+		return RedirectToAction("Index", "Home");
+	}
+
+	public IActionResult Index()
+	{
+		return View();
+	}
 }
