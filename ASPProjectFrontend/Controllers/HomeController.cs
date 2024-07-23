67e04c7d4cfc9da393e91237bcc5631403f08a3e
Tobias Skog <t_ott_e@hotmail.com>
2024-07-23 13:07:59 +0200

    prepared for the backend

--- ASPProjectFrontend/Controllers/HomeController.cs
+++ ASPProjectFrontend/Controllers/HomeController.cs
@@ -6,27 +6,23 @@ using System.Diagnostics;
 namespace ASPProjectFrontend.Controllers;
 public class HomeController(ApiServices apiServices, ILogger<HomeController> logger) : Controller
 {
-  private readonly ApiServices _api = apiServices;
-  private readonly ILogger<HomeController> _logger = logger;
+    private readonly ApiServices _api = apiServices;
+    private readonly ILogger<HomeController> _logger = logger;
 
-  public IActionResult Index()
-  {
-    List<Game> games = new(){
-        {new(){Id = 1, Name = "Example Game 1", ShortDescription = "This is a short description of the example game.", HeaderImage = "path-to-image.jpg", InitialPrice = 59, DiscountPercent = 20}},
-        {new() {Id = 2, Name = "Example Game 2", ShortDescription = "This is a short description of the example game.", HeaderImage = "path-to-image.jpg", InitialPrice = 59, DiscountPercent = 20}},
-        {new() {Id = 3, Name = "Example Game 3", ShortDescription = "This is a short description of the example game.", HeaderImage = "path-to-image.jpg", InitialPrice = 59, DiscountPercent = 20}}
-      };
-    return View(games);
-  }
+    public async Task<IActionResult> Index()
+    {
+        var games = await _api.GetAllGames();
+        return View(games);
+    }
 
-  public IActionResult Privacy()
-  {
-    return View();
-  }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-  public IActionResult Error()
-  {
-    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-  }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
 }
